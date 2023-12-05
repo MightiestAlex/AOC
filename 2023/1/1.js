2d92f702a7162e0a6ca020712ff0f9b3d4505321
MightiestAlex <alx@hotmail.co.uk>
2023-12-05 01:30:22 +0000

    complete

--- 2023/1/1.js
+++ 2023/1/1.js
@@ -22,21 +22,15 @@ fs.readFile('fkdata', 'utf8', (err, data) => {
 
     unfiltered = data.replaceAll(/\s/g,'-').split('-');
 
-    console.log(unfiltered.length);
-
     for(let i=0; i<unfiltered.length; i++){
-
 
 	for(key in info){
 	    unfiltered[i] = unfiltered[i].replaceAll(key, info[key]);
 	}
 	
 	unfiltered[i] = unfiltered[i].replaceAll(/\D/g, '');
-    
-	console.log(unfiltered);
-
+  
 	if(unfiltered[i].length < 2) {
-	    console.log(unfiltered[i])
 	    
 	    total += +unfiltered[i].slice(0, 1)*10
 	    total += +unfiltered[i].slice(0, 1);
@@ -44,13 +38,8 @@ fs.readFile('fkdata', 'utf8', (err, data) => {
 	}
 
 	total += +unfiltered[i].slice(0, 1)*10
-	total += +unfiltered[i].slice(-1);
-	
-	    //break;
-	
+	total += +unfiltered[i].slice(-1);		
     }
-
-   //console.log(total)
-
-
+    
+   console.log(total)
 })
